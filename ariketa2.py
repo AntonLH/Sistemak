5b56dbcf0e3b68f506ceff53d5dfa35f67d85e46
AntonLH <anton.larrahi@gmail.com>
2016-11-29 10:29:41 +0100

    commit

--- ariketa2.py
+++ ariketa2.py
@@ -2,7 +2,7 @@ import os
 os.makedirs('/home/antonlarrazabal/Deskargak/tmp/kopia')
 for filename in os.listdir('/home/antonlarrazabal/Deskargak'):
      if os.path.getsize(os.path.join('/home/antonlarrazabal/Deskargak', filename))>=10240 and os.path.getsize(os.path.join('/home/antonlarrazabal/Deskargak', filename))<= 10485760 and os.path.isfile(os.path.join('/home/antonlarrazabal/Deskargak', filename)):
-	#os.system('cp '+filename+' Deskargak/tmp/kopia/'+filename)
+	shutil.copy(filename, 'Deskargak/tmp/kopia/'+filename)
         print("sartu da: " + filename)
 
      else:
